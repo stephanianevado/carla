107ecb11ec931e6e2f5590af7d1b11ae022c13ae
Stephania Nevado <stephania.arantxa@gmail.com>
2022-03-29 22:10:04 +0200

    configure next and ts

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+module.exports = {
+  images: {
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    domains: ['cdn2.thecatapi.com'],
+  },
+}
