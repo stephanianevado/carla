33f2b01071c87f08814ce6174594a38c053cc3a6
Stephania Nevado <stephania.arantxa@gmail.com>
2022-03-29 22:11:12 +0200

    add document

---
+++ pages/_document.tsx
@@ -0,0 +1,20 @@
+import { Head, Html, Main, NextScript } from 'next/document'
+
+const Document = () => {
+  return (
+    <Html>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
+
+export default Document
