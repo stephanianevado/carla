4e7630aa20e9d21287aff71f7dfd0e31121d84a2
Stephania Nevado <stephania.arantxa@gmail.com>
2022-03-29 22:18:28 +0200

    add P

---
+++ components/common/P/P.tsx
@@ -0,0 +1,16 @@
+import { Theme,ThemeColor } from 'components/common/Theme'
+import styled from 'styled-components'
+
+export type PProps = {
+  color?: ThemeColor
+}
+
+export const P = styled.p<PProps>`
+  color: ${({ color }) =>
+    color ? Theme.colors[color] : Theme.colors.darkBlue};
+  overflow-wrap: break-word;
+  font-size: ${Theme.fontSize.medium};
+  line-height: ${Theme.lineHeight.large};
+  font-weight: 300;
+  letter-spacing: 0.1rem;
+`
