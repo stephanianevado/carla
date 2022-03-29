4204457ba59a39efb63c902625fa81bd2b76a71f
Stephania Nevado <stephania.arantxa@gmail.com>
2022-03-29 22:18:23 +0200

    add Paper

---
+++ components/common/Paper/Paper.tsx
@@ -0,0 +1,12 @@
+import { Theme } from 'components/common/Theme'
+import styled from 'styled-components'
+
+export const Paper = styled.div`
+  border-radius: 1rem;
+  background-color: ${Theme.colors.white};
+  box-shadow: 0px 0px 50px rgba(64, 64, 168, 0.15);
+  padding: 1rem;
+  ${Theme.breakpoints.laptop} {
+    padding: 3rem;
+  }
+`
