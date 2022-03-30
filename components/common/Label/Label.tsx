e25fd805c22d5ba344c6004f04d2135544812b80
Stephania Nevado <stephania.arantxa@gmail.com>
2022-03-30 21:27:34 +0200

    refactor Label

--- components/common/Label/Label.tsx
+++ components/common/Label/Label.tsx
@@ -1,10 +1,13 @@
-import { MarginProps } from 'components/common/styleUtils'
-import { PropsWithChildren } from 'react'
-import { StyledLabel } from './Label.style'
+import { MarginProps, styledMargins } from 'components/common/styleUtils'
+import { Theme } from 'components/common/Theme'
+import styled from 'styled-components'
 
-export const Label = ({
-  children,
-  ...props
-}: PropsWithChildren<MarginProps>) => {
-  return <StyledLabel {...props}>{children}</StyledLabel>
-}
+export const Label = styled.span<MarginProps>`
+  padding: 0.5rem;
+  border-radius: 0.75rem;
+  background-color: ${Theme.colors.wildRose};
+  color: ${Theme.colors.dandelion};
+  font-weight: ${Theme.fontWeight.bold};
+  font-size: ${Theme.fontSize.small};
+  ${styledMargins}
+`
