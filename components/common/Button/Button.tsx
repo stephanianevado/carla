f2b745fd5dc33086dc9047481dc46cb0639c6888
Stephania Nevado <stephania.arantxa@gmail.com>
2022-03-30 20:47:42 +0200

    refactor Button

--- components/common/Button/Button.tsx
+++ components/common/Button/Button.tsx
@@ -1,37 +1,29 @@
 import { Theme } from 'components/common/Theme'
-import { PropsWithChildren } from 'react'
-import { StyledButton } from './Button.style'
+import { MouseEventHandler, PropsWithChildren } from 'react'
+import styled from 'styled-components'
 
 export type ButtonProps = PropsWithChildren<{
   color?: string
   bgColor?: string
   hover?: string
   disabled?: boolean
-  onClick: () => void
+  onClick: MouseEventHandler<HTMLButtonElement>
 }>
 
-export const Button = ({
-  color = Theme.colors.white,
-  bgColor = Theme.colors.blue,
-  hover = Theme.colors.deepBlue,
-  disabled = false,
-  onClick,
-  children,
-  ...props
-}: ButtonProps) => (
-  <StyledButton
-    color={color}
-    bgColor={bgColor}
-    hover={hover}
-    disabled={disabled}
-    onClick={(e: any) => {
-      e.preventDefault()
-      if (onClick) {
-        onClick()
-      }
-    }}
-    {...props}
-  >
-    {children}
-  </StyledButton>
-)
+export const Button = styled.button<ButtonProps>`
+  font-size: ${Theme.fontSize.normal};
+  font-weight: ${Theme.fontWeight.semiBold};
+  cursor: pointer;
+  padding: 1rem 2rem;
+  color: ${(props) => props.color || Theme.colors.white};
+  background: ${(props) => props.bgColor || Theme.colors.blue};
+  border: 0.063rem solid ${Theme.colors.white};
+  box-sizing: border-box;
+  border-radius: 0.5rem;
+  &:hover {
+    background: ${(props) => props.hover || Theme.colors.cornBlue};
+  }
+  &:disabled {
+    opacity: 0.4;
+  }
+`
