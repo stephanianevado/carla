def79fa05a0a35827c926ecebbeb7a92f9cf1c02
Stephania Nevado <stephania.arantxa@gmail.com>
2022-03-30 21:29:40 +0200

    increase Paper shadow

--- components/common/Paper/Paper.tsx
+++ components/common/Paper/Paper.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 export const Paper = styled.div`
   border-radius: 1rem;
   background-color: ${Theme.colors.white};
-  box-shadow: 0px 0px 50px rgba(64, 64, 168, 0.15);
+  box-shadow: 0px 0px 50px rgba(64, 64, 168, 0.5);
   padding: 1rem;
   ${Theme.breakpoints.laptop} {
     padding: 3rem;
