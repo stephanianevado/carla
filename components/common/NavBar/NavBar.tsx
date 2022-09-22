4d2baf7d65ad8769ce09c52781105e4577c9536e
Stephania Nevado <stephania.arantxa@gmail.com>
2022-09-22 19:29:46 +0200

    fix logo

--- components/common/NavBar/NavBar.tsx
+++ components/common/NavBar/NavBar.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import Logo from 'public/logo.svg'
+import Logo from 'public/Logo.svg'
 import { HamburgerMenu } from './HamburgerMenu'
 import { Menu, Nav } from './NavBar.style'
 import { NavItems } from './NavItems'
